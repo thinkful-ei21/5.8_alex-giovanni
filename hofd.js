a21669282023a55718204d768192ebdb0b523de1
Alex Widner <manny1806@gmail.com>
2018-05-08 11:16:48 -0700

    functions arg 2

--- hofd.js
+++ hofd.js
@@ -10,5 +10,25 @@ function goodbye(){console.log('Goodbye world')};
 //repeat(hello, 5);
 //repeat(goodbye, 5);
 
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+    let newArr = [];
+    for (let x = 0; x < arr.length; x++){
+        if(fn(arr[x])){newArr.push(arr[x])}
+    }
+    return newArr;
+}
 
 
